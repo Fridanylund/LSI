9155bccc8490fd22339b0a8b8662c3d6c4dd69b5
Frida <Frida@FRIDAS-DATOR>
2016-11-04 11:51:12 +0100

    A baby blue GUI with some new features

--- LSI Project GUI/LSI Project GUI/GeneratedFiles/ui_LSIProjectGUI.h
+++ LSI Project GUI/LSI Project GUI/GeneratedFiles/ui_LSIProjectGUI.h
@@ -17,7 +17,9 @@
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -31,6 +33,13 @@ public:
     QPushButton *pushButton;
     QLabel *im_show;
     QLabel *label_2;
+    QPushButton *startButton;
+    QPushButton *stopButton;
+    QPushButton *createROIButton;
+    QPushButton *removeROIButton;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QPlainTextEdit *plainTextEdit;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -39,23 +48,46 @@ public:
     {
         if (LSIProjectGUIClass->objectName().isEmpty())
             LSIProjectGUIClass->setObjectName(QStringLiteral("LSIProjectGUIClass"));
-        LSIProjectGUIClass->resize(600, 400);
+        LSIProjectGUIClass->resize(928, 557);
         centralWidget = new QWidget(LSIProjectGUIClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         pushButton = new QPushButton(centralWidget);
         pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(210, 20, 171, 71));
+        pushButton->setGeometry(QRect(70, 20, 171, 71));
         im_show = new QLabel(centralWidget);
         im_show->setObjectName(QStringLiteral("im_show"));
         im_show->setEnabled(true);
-        im_show->setGeometry(QRect(70, 130, 191, 171));
+        im_show->setGeometry(QRect(50, 100, 191, 171));
         label_2 = new QLabel(centralWidget);
         label_2->setObjectName(QStringLiteral("label_2"));
-        label_2->setGeometry(QRect(340, 120, 181, 171));
+        label_2->setGeometry(QRect(80, 270, 111, 61));
+        startButton = new QPushButton(centralWidget);
+        startButton->setObjectName(QStringLiteral("startButton"));
+        startButton->setGeometry(QRect(410, 80, 141, 61));
+        stopButton = new QPushButton(centralWidget);
+        stopButton->setObjectName(QStringLiteral("stopButton"));
+        stopButton->setGeometry(QRect(410, 150, 141, 61));
+        createROIButton = new QPushButton(centralWidget);
+        createROIButton->setObjectName(QStringLiteral("createROIButton"));
+        createROIButton->setGeometry(QRect(410, 220, 141, 61));
+        removeROIButton = new QPushButton(centralWidget);
+        removeROIButton->setObjectName(QStringLiteral("removeROIButton"));
+        removeROIButton->setGeometry(QRect(410, 300, 141, 61));
+        scrollArea = new QScrollArea(centralWidget);
+        scrollArea->setObjectName(QStringLiteral("scrollArea"));
+        scrollArea->setGeometry(QRect(620, 90, 171, 161));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QStringLiteral("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 169, 159));
+        plainTextEdit = new QPlainTextEdit(scrollAreaWidgetContents);
+        plainTextEdit->setObjectName(QStringLiteral("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(10, 10, 151, 141));
+        scrollArea->setWidget(scrollAreaWidgetContents);
         LSIProjectGUIClass->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(LSIProjectGUIClass);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 600, 21));
+        menuBar->setGeometry(QRect(0, 0, 928, 21));
         LSIProjectGUIClass->setMenuBar(menuBar);
         mainToolBar = new QToolBar(LSIProjectGUIClass);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -75,6 +107,10 @@ public:
         pushButton->setText(QApplication::translate("LSIProjectGUIClass", "Push Me!!", 0));
         im_show->setText(QApplication::translate("LSIProjectGUIClass", "Somthing might happen here!", 0));
         label_2->setText(QString());
+        startButton->setText(QApplication::translate("LSIProjectGUIClass", "Start", 0));
+        stopButton->setText(QApplication::translate("LSIProjectGUIClass", "Stop", 0));
+        createROIButton->setText(QApplication::translate("LSIProjectGUIClass", "create ROI", 0));
+        removeROIButton->setText(QApplication::translate("LSIProjectGUIClass", "remove ROI", 0));
     } // retranslateUi
 
 };
